659bab26346e0db85f009e3e0ad663bf449a5737
jjgdelgado <jjgalandelgado@gmail.com>
2021-04-16 14:50:03 -0500

    fix crud

---
+++ src/main/java/com/bootcamp/avengers/PreferenceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.avengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreferenceApplication.class, args);
+	}
+
+}
